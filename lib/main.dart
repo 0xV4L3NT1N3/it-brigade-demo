52d11ab9230c5003d948461641ada9eedd36555c
V4L3NT1N3 <nicholaschn@protonmail.com>
2021-04-02 16:29:05 +0800

    Added profile image

--- lib/main.dart
+++ lib/main.dart
@@ -17,70 +17,154 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         // top appBar
-        appBar: AppBar(title: Text('Facebook', style: TextStyle(color: Colors.black),), backgroundColor: Colors.grey[50], actions: [
-          Switch(value: false, onChanged: null, inactiveThumbColor: Colors.black, )
-        ],),
+        appBar: AppBar(
+          title: Text(
+            'Facebook',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.grey[50],
+          actions: [
+            Switch(
+              value: false,
+              onChanged: null,
+              inactiveThumbColor: Colors.black,
+            )
+          ],
+        ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               // name, title and profile photo
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CircleAvatar(radius: 25,),
-                  SizedBox(width: 15,),
+                  CircleAvatar(
+                    radius: 43,
+                    backgroundColor: Colors.blueGrey,
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.grey[50],
+                      child: CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage('images/profile.jpg'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Zark Muckberg', style: TextStyle(fontSize: 18),),
-                      Text('President'),
+                      Text(
+                        'Zark Muckberg',
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                      Text(
+                        'President',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
                     ],
                   ),
                 ],
               ),
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               // interesting stats
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Column(
                     children: [
-                      Text('24', style: TextStyle(fontSize: 20),),
+                      Text(
+                        '24',
+                        style: TextStyle(fontSize: 20),
+                      ),
                       Text('Duty Days'),
                     ],
                   ),
                   Column(
                     children: [
-                      Text('34', style: TextStyle(fontSize: 20),),
+                      Text(
+                        '34',
+                        style: TextStyle(fontSize: 20),
+                      ),
                       Text('Bugs Solved'),
                     ],
                   ),
                   Column(
                     children: [
-                      Text('4', style: TextStyle(fontSize: 20),),
+                      Text(
+                        '4',
+                        style: TextStyle(fontSize: 20),
+                      ),
                       Text('Hours Slept'),
                     ],
                   ),
                 ],
               ),
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               Padding(
                 padding: const EdgeInsets.only(left: 15.0),
                 child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Dishonourable Awards', style: TextStyle(fontSize: 22),)),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Dishonourable Awards',
+                      style: TextStyle(fontSize: 22),
+                    )),
               ),
-              SizedBox(height: 15,),
+              SizedBox(
+                height: 15,
+              ),
               // achievement cards
               ListView(
                 shrinkWrap: true,
                 children: [
-                  Card(child: ListTile(leading: Icon(Icons.music_note_rounded, color: Colors.pinkAccent, size: 35,), title: Text('Rock Star'), subtitle: Text('Blasted music through the lab speaker'),),),
-                  Card(child: ListTile(leading: Icon(Icons.restaurant_menu, color: Colors.green, size: 35,), title: Text('Master Muncher'), subtitle: Text('Ate in the lab more than twice'),),),
-                  Card(child: ListTile(leading: Icon(Icons.laptop_chromebook_rounded, color: Colors.lightBlueAccent, size: 35,), title: Text('Totally Not-On-Purpose'), subtitle: Text('Destroyed more than 3 lab PCs'),),),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.music_note_rounded,
+                        color: Colors.pinkAccent,
+                        size: 35,
+                      ),
+                      title: Text('Rock Star'),
+                      subtitle: Text('Blasted music through the lab speaker'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.restaurant_menu,
+                        color: Colors.green,
+                        size: 35,
+                      ),
+                      title: Text('Master Muncher'),
+                      subtitle: Text('Ate in the lab more than twice'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.laptop_chromebook_rounded,
+                        color: Colors.lightBlueAccent,
+                        size: 35,
+                      ),
+                      title: Text('Totally Not-On-Purpose'),
+                      subtitle: Text('Destroyed more than 3 lab PCs'),
+                    ),
+                  ),
                 ],
-                ),
+              ),
             ],
           ),
         ),
