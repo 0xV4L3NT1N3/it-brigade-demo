f2b880833af36ccc18bf2d1e1581f9c9fd74de9d
V4L3NT1N3 <nicholaschn@protonmail.com>
2021-04-02 16:56:56 +0800

    Styled achievement cards

--- lib/main.dart
+++ lib/main.dart
@@ -31,106 +31,108 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              // name, title and profile photo
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 43,
-                    backgroundColor: Colors.blueGrey,
+        body: ListView(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            // name, title and profile photo
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 43,
+                  backgroundColor: Colors.blueGrey,
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey[50],
                     child: CircleAvatar(
-                      radius: 40,
-                      backgroundColor: Colors.grey[50],
-                      child: CircleAvatar(
-                        radius: 35,
-                        backgroundImage: AssetImage('images/profile.jpg'),
-                      ),
+                      radius: 35,
+                      backgroundImage: AssetImage('images/profile.jpg'),
                     ),
                   ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Zark Muckberg',
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Zark Muckberg',
+                      style: TextStyle(
+                        fontSize: 22,
                       ),
-                      Text(
-                        'President',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
+                    ),
+                    Text(
+                      'President',
+                      style: TextStyle(
+                        fontSize: 16,
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              // interesting stats
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        '24',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Duty Days'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        '34',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Bugs Solved'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        '4',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Hours Slept'),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Dishonourable Awards',
-                      style: TextStyle(fontSize: 22),
-                    )),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              // achievement cards
-              ListView(
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            // interesting stats
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '24',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Duty Days'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '34',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Bugs Solved'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '4',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text('Hours Slept'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Dishonourable Awards',
+                    style: TextStyle(fontSize: 22),
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            // achievement cards
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
                 shrinkWrap: true,
                 children: [
                   Card(
+                    elevation: 3,
                     child: ListTile(
                       leading: Icon(
                         Icons.music_note_rounded,
@@ -142,6 +144,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   Card(
+                    elevation: 3,
                     child: ListTile(
                       leading: Icon(
                         Icons.restaurant_menu,
@@ -153,6 +156,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   Card(
+                    elevation: 3,
                     child: ListTile(
                       leading: Icon(
                         Icons.laptop_chromebook_rounded,
@@ -163,11 +167,23 @@ class MyApp extends StatelessWidget {
                       subtitle: Text('Destroyed more than 3 lab PCs'),
                     ),
                   ),
+                  Card(
+                    elevation: 3,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.local_fire_department,
+                        color: Colors.deepOrangeAccent,
+                        size: 35,
+                      ),
+                      title: Text('Sharp Shooter'),
+                      subtitle: Text('Victorious in a 5v1 CS 1.6 match'),
+                    ),
+                  ),
                 ],
               ),
-            ],
-          ),
-        ),
+            ),
+          ],
+        )
       ),
     );
   }
